b4baaec778edd13c612248bb5a442aad8e4bb7eb
Patrick Narciso <patrick.ncsilva@gmail.com>
2022-04-30 20:23:35 -0300

    feat(components): finishes autocomplete feature with handle and blur events

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { Autocomplete } from './components';
 import { getUsers } from './services/users';
 
 const App: React.FC = () => {
+  const [users, setUsers] = useState<string[]>([]);
+
   useEffect(() => {
-    getUsers().then((response) => console.log(response));
+    const loadUsers = async () => {
+      const users = await getUsers();
+      setUsers(users.map(({ email }) => email));
+    }
+    loadUsers();
   }, []);
 
-  return <Autocomplete />
+  return <Autocomplete data={users} placeholder="Search for email user" />
 };
 
 export default App;
--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { AutocompleteInput } from './styles';
 
-const Autocomplete: React.FC = () => {
+interface Props {
+  data: string[],
+  placeholder?: string,
+  className?: string,
+}
 
-  return <AutocompleteInput value="text" />;
+const Autocomplete: React.FC<Props> = ({ data, className, placeholder = '' }) => {
+  const [text, setText] = useState<string>('');
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  const onChangeHandler = (text: HTMLInputElement["value"]) => {
+    if (!text.length) {
+      setText('');
+      setSuggestions([]);
+      return false;
+    };
+    const matches = data.filter((value) => {
+      const regex = new RegExp(`${text}`, "gi");
+      return value.match(regex);
+    });
+    setSuggestions(matches);
+    setText(text);
+  };
+
+  const onBlurHandler = () => {
+    setTimeout(() => {
+      setSuggestions([]);
+    }, 100);
+  }
+
+  return (
+    <div>
+      <AutocompleteInput
+        className={className}
+        placeholder={placeholder}
+        value={text}
+        onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+          onChangeHandler(event.target.value)
+        }
+        onBlur={onBlurHandler}
+      />
+      {suggestions && suggestions.map((suggestion, index) => <div key={`${suggestion}-${index}`}>{suggestion}</div>)}
+    </div>
+  );
 }
 
 export default Autocomplete;
